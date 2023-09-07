155474bf5318f4911a05a37bbf81e8a326188fc5
litesh1123 <139573484+litesh1123@users.noreply.github.com>
2023-09-07 18:41:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "k3s-cluster-provision5"
+    bucket         = "k3s-task5-backend"
     key            = "terraform/state.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-table" 
